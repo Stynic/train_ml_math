ff670cea527cb8b3114393fe8c2a8fdc0b1d145d
S s <63148490+Stynic@users.noreply.github.com>
2022-01-17 14:55:13 +0300

    complete module 8
    complete module 8

--- main.py
+++ main.py
@@ -1,19 +1,45 @@
-def sum_range(n, step):
-    if n <= 0:
-        return 0
-    result = sum_range(n - step, step)
-    return n + result
+class Fraction:
+    def __init__(self, num: int or str, dem: int or str = 0):
+        self.fraction = num + dem / 10 if num > 0 else num - dem / 10
+
+    def __str__(self):
+        return str(self.fraction)
+
+    def __int__(self):
+        return int(self.fraction)
+
+    def __float__(self):
+        return self.fraction
+
+    def __repr__(self):
+        return str(self.fraction)
+
+    def __add__(self, other):
+        _num, _dem = str(round(self.fraction + other.fraction, 1)).split('.')
+        return Fraction(int(_num), int(_dem))
+
+    def __sub__(self, other):
+        _num, _dem = str(round(self.fraction - other.fraction, 1)).split('.')
+        return Fraction(int(_num), int(_dem))
+
+    def __mul__(self, other):
+        _num, _dem = str(round(self.fraction * other.fraction, 1)).split('.')
+        return Fraction(int(_num), int(_dem))
+
+    def __truediv__(self, other):
+        _num, _dem = str(round(self.fraction / other.fraction, 1)).split('.')
+        return Fraction(int(_num), int(_dem))
 
 
-def factorial(n: str | int):
-    if n == 0:
-        return 1
-    n_minus_1 = factorial(n - 1)
-    return n * n_minus_1
+class OperationsOnFraction(Fraction):
+    def getint(self):
+        return int(self)
+
+    def getfloat(self):
+        return float(self)
 
 
 if __name__ == '__main__':
-    print(factorial(5))
-    # print(sum_range(5, 1))
-    # print(sum_range(5, 9))
-    # print(sum_range(8, 2))
+    print(Fraction(3, 1) + Fraction(2, 8))
+    print(Fraction(4, 7) + Fraction(3, 1))
+
